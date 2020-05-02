b63f7adfb8477029ef12bacafa0d332d4fbb5099
Shehab Tweel <shehabtweel@gmail.com>
2020-05-02 22:34:16 +0200

    Add tslint ignore lines

--- src/sum.ts
+++ src/sum.ts
@@ -2,9 +2,9 @@ export function sum(numbers: number[]) {
   if (numbers.length <= 1) {
     return new Error('Not enough numbers');
   }
-  let result: number = 0;
-  numbers.forEach(number => {
+  let result: number = 0; // tslint:disable-line
+  for (const number of numbers) { // tslint:disable-line
     result += number;
-  });
+  }
   return result;
 }
