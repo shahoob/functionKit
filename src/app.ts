f1e19e546d8c2813ac6822809d5262efb3998783
Shehab Tweel <shehabtweel@gmail.com>
2020-05-11 01:58:20 +0200

    Remove array functions

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import { sum } from "./sum";
 import { subtract } from "./subtract";
 import { divide } from "./divide";
 import { greet } from "./greeter";
-import * as arrayFunctions from "./arrayFunctions";
 
 module.exports = {
   sum,
   subtract,
   divide,
-  greet,
-  arrayFunctions
+  greet
 }
