15bd7aff9d09c3a0d6c015bb91da50243895524a
Shehab Tweel <shehabtweel@gmail.com>
2020-05-16 13:36:30 +0200

    Update sum.ts to use .reduce() function

--- src/sum.ts
+++ src/sum.ts
@@ -2,9 +2,8 @@ export function sum(numbers: number[]) {
   if (numbers.length <= 1) {
     return new Error('Not enough numbers');
   }
-  let result: number = 0; // tslint:disable-line
-  for (const number of numbers) { // tslint:disable-line
-    result += number;
-  }
+  let result = numbers.reduce((current, num) => {
+    return current += num;
+  });
   return result;
 }
