4e095d5a1f9f3b229f3b688a53321bcc1a9a75bb
Shehab Tweel <shehabtweel@gmail.com>
2020-05-16 13:17:16 +0200

    Fix exports in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import { sum } from "./sum";
 import { subtract } from "./subtract";
 import { divide } from "./divide";
 import { greet } from "./greeter";
+import { calcPercent } from "./calcPercent";
+import { multiply } from "./multiply"
 
 module.exports = {
   sum,
   subtract,
   divide,
-  greet
+  greet,
+  calcPercent,
+  multiply
 }
