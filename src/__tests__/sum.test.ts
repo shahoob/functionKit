84a8619a3b3291c76b1033952502f1490391e500
Shehab Tweel <shehabtweel@gmail.com>
2020-10-09 01:10:14 +0200

    Remove the uneeded test suite from sum.test.ts

--- src/__tests__/sum.test.ts
+++ src/__tests__/sum.test.ts
@@ -3,8 +3,3 @@ import { sum } from '../sum';
 test('sum 2 numbers', () => {
   expect(sum([1, 1])).toBe(2);
 });
-
-test('make an error', () => {
-  expect(sum([1])).not.toBe(2);
-  expect(sum([1])).toStrictEqual(new Error('Not enough numbers'));
-});
