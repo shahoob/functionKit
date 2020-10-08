eafb91c6bf81e0cffc29dad29595e330f970d59a
Shehab Tweel <shehabtweel@gmail.com>
2020-10-09 00:09:22 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/app.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts'],
+    }
+}
