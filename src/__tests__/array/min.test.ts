e5dbfef1a8d204537644e3519a4e94081bd3fec5
Shehab Tweel <shehabtweel@gmail.com>
2020-11-13 10:38:06 +0200

    fix tests again

--- src/__tests__/array/min.test.ts
+++ src/__tests__/array/min.test.ts
@@ -4,15 +4,15 @@ import { range } from '../../range';
 import { randomize } from '../../array/randomize';
 
 test('Get the smallest number in a range array', () => {
-    expect(min(range(1, 100))).toBe(100);
+    expect(min(range(1, 100))).toBe(1);
 });
 
 test('Get the smallest number in a reversed array', () => {
-    expect(min(range(1, 100).reverse())).toBe(100);
+    expect(min(range(1, 100).reverse())).toBe(1);
 });
 
 test('Get the smallest number in a randomized array', () => {
     expect(min(
         randomize(range(1, 100))
-    )).toBe(100);
+    )).toBe(1);
 });
